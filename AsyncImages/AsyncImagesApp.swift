0e1dae2d9a8c1ab6d9e6d1383a1562524c4de8fc
Otavio Miguel <otavio.miguel@amaro.com>
2023-01-19 10:01:06 -0300

    Initial Commit

---
+++ AsyncImages/AsyncImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImagesApp.swift
+//  AsyncImages
+//
+//  Created by Otavio Miguel on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
